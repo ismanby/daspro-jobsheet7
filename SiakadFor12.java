5faabc26862ff639cdce7a5d352359b6398c674d
ismanby <ismaanby@gmail.com>
2025-10-23 13:14:41 +0700

    Modifikasi Percobaan 1

--- SiakadFor12.java
+++ SiakadFor12.java
@@ -5,6 +5,7 @@ public class SiakadFor12 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidak_lulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -17,8 +18,16 @@ public class SiakadFor12 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidak_lulus++;
+            }
         }
 
+        System.out.println("Jumlah mahasiswa yang lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + tidak_lulus);
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
 
